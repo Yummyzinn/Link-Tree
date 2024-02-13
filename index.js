6e665ecd3fd3c36c8455308a5bc5a76e25f0e9e3
Yummyzinn <157983636+Yummyzinn@users.noreply.github.com>
2024-02-13 18:41:21 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function sla() {
+
+
+let element = document.body;
+element.classList.toggle("dark-mode");
+
+var botao = document.getElementById("butao")
+botao.style.backgroundColor.toggle ='white'
+botao.style.color.toggle = 'black'
+
+}
